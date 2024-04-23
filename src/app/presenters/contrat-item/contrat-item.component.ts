713fc74c95c015fbca88cda2698432128ba1ab16
Jeremy Chong <jeremy.chong@octo.com>
2024-04-23 18:35:03 +0200

    install font-awesome + icons dynamically

--- src/app/presenters/contrat-item/contrat-item.component.ts
+++ src/app/presenters/contrat-item/contrat-item.component.ts
@@ -5,8 +5,6 @@ import {
   Input,
   ViewChild,
 } from "@angular/core";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { faFileContract, faPen } from "@fortawesome/free-solid-svg-icons";
 import {
   DonneesContrat,
   ItemContrat,
@@ -17,13 +15,11 @@ import { ContratService } from "../../services/contrat.service";
 @Component({
   selector: "app-contrat-item",
   standalone: true,
-  imports: [FontAwesomeModule, CommonModule],
+  imports: [CommonModule],
   templateUrl: "./contrat-item.component.html",
   styleUrl: "./contrat-item.component.scss",
 })
 export class ContratItemComponent implements AfterViewInit {
-  faFileContract = faFileContract;
-  faPen = faPen;
   @ViewChild("btnModifier") buttonElement!: ElementRef;
 
   @Input() donneeContrat!: DonneesContrat;
